835e33322310c9b9fa8f6b3a07fbf4b5df5a5ebf
JayTalekar <jaytalekar.developer@gmail.com>
2020-09-26 00:36:04 +0530

    Removed Data Binding from News Detail Fragment.

--- app/src/main/java/com/jaytalekar/newsforyou/NewsDetail/NewsDetail.kt
+++ app/src/main/java/com/jaytalekar/newsforyou/NewsDetail/NewsDetail.kt
@@ -4,19 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.jaytalekar.newsforyou.R
-import com.jaytalekar.newsforyou.databinding.FragmentNewsDetailBinding
 
 class NewsDetail : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding : FragmentNewsDetailBinding = DataBindingUtil
-            .inflate(inflater, R.layout.fragment_news_detail, container, false)
+        // Inflate the Fragment as usual
+        val rootView = LayoutInflater.from(this.activity)
+            .inflate(R.layout.fragment_news_detail, container, false)
 
-        return binding.root
+        return rootView
     }
 
 }
