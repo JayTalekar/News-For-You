aaf052995e87d1ee7aec28efe50f2fe02343ef05
JayTalekar <jaytalekar.developer@gmail.com>
2020-10-24 20:45:03 +0530

    Updated Database DAO for further use.

--- app/src/main/java/com/jaytalekar/newsforyou/database/NewsDatabaseDao.kt
+++ app/src/main/java/com/jaytalekar/newsforyou/database/NewsDatabaseDao.kt
@@ -9,16 +9,22 @@ import androidx.room.Query
 interface NewsDatabaseDao {
 
     @Insert
-    fun insert(favouriteNews: FavouriteNews)
+    fun insert(favouriteNews: FavouriteNews) : Long
 
     @Query("SELECT * FROM favourite_news_table WHERE news_Id = :newsId")
     fun get(newsId: Long) : FavouriteNews
+
+    @Query("SELECT * FROM favourite_news_table WHERE article_Url = :articleUrl")
+    fun get(articleUrl: String) : FavouriteNews
 
     @Query("SELECT * FROM favourite_news_table ORDER BY news_Id DESC")
     fun getAllNews() : LiveData<List<FavouriteNews>>
 
     @Query("DELETE FROM favourite_news_table WHERE news_Id = :newsId")
     fun delete(newsId : Long)
+
+    @Query("DELETE FROM favourite_news_table WHERE article_Url = :articleUrl")
+    fun delete(articleUrl : String)
 
     @Query("DELETE FROM favourite_news_table")
     fun clear()
